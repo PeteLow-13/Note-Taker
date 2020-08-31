9a08190e0321fe7f9fad4580aff5a82d97444bf2
Pete Low <pedritolow@gmail.com>
2020-08-30 20:32:41 -0700

    added server and notes data base

---
+++ db/notesdatabase.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path')
+
+class NotesDatabase{
+    constructor(){
+
+    }
+    static getNotes() {
+        var notes = [];
+        notes = fs.readFileSync(path.join(__dirname +'/db.json'));
+        return JSON.parse(notes); 
+    }
+    static addNote(title,text){
+        
+        var highestId = 0;
+        var notes = JSON.parse(fs.readFileSync(path.join(__dirname +'/db.json')));
+        notes.forEach(function(note){
+            if (note.id > highestId){
+                highestId = note.id;
+            };
+        });
+        var note = {'id': ++highestId, 'title': title, 'text': text};
+        notes.push(note);
+        fs.writeFileSync(path.join(__dirname+ '/db.json'), JSON.stringify(notes));
+
+        return note;
+    }
+    static deleteNote(id){
+        var notes = JSON.parse(fs.readFileSync(path.join(__dirname +'/db.json')));
+        notes.forEach(function(note, index){
+            if (note.id == id){
+                notes.splice(index, 1);
+            }
+        })
+        fs.writeFileSync(path.join(__dirname+ '/db.json'), JSON.stringify(notes));
+    }
+
+
+}; 
+
+
+module.exports = NotesDatabase;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const path = require('path');
+const NotesDatabase = require('./db/NotesDatabase');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname +'/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(NotesDatabase.getNotes());
+});
+
+app.post('/api/notes', (req, res) => {
+    res.json(NotesDatabase.addNote(req.body.title, req.body.text));
+});
+
+app.delete('/api/notes/:id', (req, res) =>{
+    NotesDatabase.deleteNote(req.params.id);
+    res.status(204).send();
+    
+});
+
+app.get('/assets/js/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname +'/public/assets/js/index.js'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname +'/public/index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log("example app listening");
+});
