1e70d70e368395a7c8d57c7666bf005ce5360dae
Pete Low <pedritolow@gmail.com>
2020-08-31 18:17:18 -0700

    changed port for heroku deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 const path = require('path');
 const NotesDatabase = require('./db/NotesDatabase');
 
