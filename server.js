77d84956dd50442ef0759bc3f909781a6a5a4638
Pete Low <pedritolow@gmail.com>
2020-09-02 09:41:02 -0700

    changed name notesdatabase

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3001;
 const path = require('path');
-const NotesDatabase = require('./db/NotesDatabase');
+const NotesDatabase = require('./db/notesDatabase');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
