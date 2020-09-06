402c63f5d024cd2ba5813ea1eca3af23b048783a
Pete Low <pedritolow@gmail.com>
2020-09-06 16:14:37 -0700

    changed casing on notes database.js path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 let PORT = process.env.PORT || 3001;
 const path = require('path');
-const NotesDatabase = require('./db/notesDatabase.js');
+const NotesDatabase = require('./db/notesdatabase.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
